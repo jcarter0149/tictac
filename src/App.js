daef014abd5674a33d2555804468758370498732
Jake Carter <jacobraycarter@gmail.com>
2018-10-11 11:15:17 -0500

    set state to null and added the fun to change the state to 'X'

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 export class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
